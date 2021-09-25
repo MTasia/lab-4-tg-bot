dba383203adf226a8061a15dde0f61a71d045d24
Taya Medvedeva <71186576+MTasia@users.noreply.github.com>
2021-09-25 14:03:40 +0300

    Add files via upload

--- error.py
+++ error.py
@@ -1,12 +1,19 @@
 import time
-
+import re
 
 def date_valid(date):
     try:
-      valid_date = time.strptime(date, '%Y-%m-%d')
-      return True
+        valid_date = time.strptime(date, '%Y-%m-%d')
+        return True
     except ValueError:
-      return False
-      
-      
+        return False
 
+def number_group_valid(number):
+    try:
+        num1, num2 = number.split("/")
+        if len(num1) == 7 and len(num2) == 5:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
--- main.py
+++ main.py
@@ -1,58 +1,143 @@
 import telebot
-import parse
-
-#parse.get_data('2021-04-12')
+from telebot import types
+import parse_data
+from datetime import date, datetime, timedelta
+import csv1
+import error
+bot = telebot.TeleBot("1786082801:AAEOePWM3kTbgLDALmK-6m46RsyLgQK1jBw")
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     keyboard = telebot.types.ReplyKeyboardMarkup(True)
-    keyboard.row('Привет', 'Пока', 'Расписание ВУЗа')
+    keyboard.row('Привет', 'Пока')
+    keyboard.row('Расписание ВУЗа')
+    chat_id = message.chat.id
     bot.send_message(message.chat.id, 'Привет!', reply_markup=keyboard)
-
-
-@bot.message_handler(commands=['test'])
-def start_message(message):
-    markup = telebot.types.InlineKeyboardMarkup()
-    markup.add(telebot.types.InlineKeyboardButton(text='СПбПУ', callback_data=1))
-    markup.add(telebot.types.InlineKeyboardButton(text='СПбГУ', callback_data=2))
-    markup.add(telebot.types.InlineKeyboardButton(text='ИТМО', callback_data=3))
-    bot.send_message(message.chat.id, text="Какой ВУЗ ты выбираешь, чтобы посмотреть распиание?", reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Ещё раз привет!')
+        bot.send_message(message.chat.id, 'Привет!')
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'Пока!')
     elif message.text == 'Расписание ВУЗа':
-        bot.send_message(message.chat.id, 'Введите дату в формате YYYY-MM-DD')
-        bot.register_next_step_handler(message, print_shedule)
-        #start_message(commands=['test'])
-def print_shedule(message):
-    t = parse.get_data(message.text)
-    for x in t:
-        bot.send_message(message.chat.id, x[0] + '\nПрепод: '+ x[1]+ '\nМесто  '+x[2])
-    bot.register_next_step_handler(message, send_text)
-            
-        
+        bot.send_message(message.chat.id, 'Введи, пожалуйста, номер группы.\n' +
+                         'Например «3630102/80401» для СПбПУ.\n' +
+                         'И только потом выбери номер группы.')
+        bot.register_next_step_handler(message, abc)
+
+        # # elif message.text == group:
+        key = types.InlineKeyboardMarkup()
+        but_1 = types.InlineKeyboardButton(text="СПбПУ", callback_data="СПбПУ")
+        # but_2 = types.InlineKeyboardButton(text="ГУАП", callback_data="ГУАП")
+        key.add(but_1)
+        bot.send_message(message.chat.id, text="Какой ВУЗ ты выбираешь, чтобы посмотреть распиание?", reply_markup=key)
 
 
 @bot.callback_query_handler(func=lambda call: True)
-def query_handler(call):
+def inline(call):
+    bot.register_next_step_handler(call.message, send_text)
+    # group = '3630102/80401'; #по идее мы это от пользователя доллжны получить
+    group_id = csv1.get_group_id(group)
+    print(group_id)
 
-    bot.answer_callback_query(callback_query_id=call.id, text='Спасибо за выбор!')
-    answer = ''
-    if call.data == '1':
-        answer = 'Расписание университета СПбПУ!'
-    elif call.data == '2':
-        answer = 'Расписание университета СПбГУ!'
-    elif call.data == '3':
-        answer = 'Расписание университета ИТМО!'
+    # if call.data == 'СПбПУ':
+    bot.send_message(call.message.chat.id, 'Твой ВУЗ - СПбПУ.')
+    key = types.InlineKeyboardMarkup()
 
-    bot.send_message(call.message.chat.id, answer)
-    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
+    but_1 = types.InlineKeyboardButton(text="Сегодня", callback_data="td_spbstu")
+    but_2 = types.InlineKeyboardButton(text="Завтра", callback_data="tw_spbstu")
+    but_3 = types.InlineKeyboardButton(text="Эта неделя", callback_data="this_week_spbstu")
+    but_4 = types.InlineKeyboardButton(text="Следующая неделя", callback_data="next_week_spbstu")
+    but_5 = types.InlineKeyboardButton(text="Ввести свою дату", callback_data="my_spbstu")
+    key.add(but_1, but_2, but_3, but_4, but_5)
+    bot.send_message(call.message.chat.id, 'Твой ВУЗ - СПбПУ.\nВыбери промежуток:', reply_markup=key)
 
+
+    # if call.data == 'ГУАП':
+    #     # bot.send_message(call.message.chat.id, 'Твой ВУЗ - ГУАП. \n Введи, пожалуйста, номер группы.')
+    #     key = types.InlineKeyboardMarkup()
+    #     but2_1 = types.InlineKeyboardButton(text="Сегодня", callback_data="td_guap")
+    #     but2_2 = types.InlineKeyboardButton(text="Завтра", callback_data="tw_guap")
+    #     but2_3 = types.InlineKeyboardButton(text="Эта неделя", callback_data="this_week_guap")
+    #     but2_4 = types.InlineKeyboardButton(text="Следующая неделя", callback_data="next_week_guap")
+    #     key.add(but2_1, but2_2, but2_3, but2_4)
+    #     bot.send_message(call.message.chat.id, 'Твой ВУЗ - ГУАП.\nВыбери промежуток:', reply_markup=key)
+
+    # формирование и вызов даты политех
+    if call.data == 'td_spbstu':
+        marker = 0
+        text = data_day_modifier(marker)
+        temp = datetime.strptime(text, '%Y-%m-%d')
+        bot.send_message(call.message.chat.id, 'Расписание на сегодня ' +
+                         "{}.{}.{}".format(temp.day, temp.month, temp.year) + ':\n\n' + print_shedule(text, group_id))
+
+    if call.data == 'tw_spbstu':
+        marker = 1
+        text = data_day_modifier(marker)
+        temp = datetime.strptime(text, '%Y-%m-%d')
+        bot.send_message(call.message.chat.id, 'Расписание на завтра ' +
+                         "{}.{}.{}".format(temp.day, temp.month, temp.year) + ':\n\n' + print_shedule(text, group_id))
+
+    if call.data == 'this_week_spbstu':
+        day_week = datetime.isoweekday(datetime.today())
+        delta = -(day_week - 1)  # чтобы узнать дату пн
+        while delta <= 7 - day_week:
+            text = data_week_modifier(delta)
+            delta += 1
+            temp = datetime.strptime(text, '%Y-%m-%d')
+            bot.send_message(call.message.chat.id, 'Расписание на текущую неделю ' +
+                             "{}.{}.{}".format(temp.day, temp.month, temp.year) + ':\n\n' + print_shedule(text,
+                                                                                                          group_id))
+
+    if call.data == 'next_week_spbstu':
+        day_week = datetime.isoweekday(datetime.today())
+        delta = 7 - day_week + 1  # чтобы узнать дату пн
+        while delta <= 6 + day_week:
+            text = data_week_modifier(delta)
+            delta += 1
+            temp = datetime.strptime(text, '%Y-%m-%d')
+            bot.send_message(call.message.chat.id, 'Расписание на следующую неделю ' +
+                             "{}.{}.{}".format(temp.day, temp.month, temp.year) + ':\n\n' + print_shedule(text,
+                                                                                                          group_id))
+
+    if call.data == 'my_spbstu':
+        msg = bot.send_message(call.message.chat.id, 'Введите дату в формате YYYY-MM-DD')
+
+
+
+
+def data_day_modifier(marker):
+    today = date.today()
+    if marker == 0:
+        return str(today)
+        # return '2021-05-08'
+
+    if marker == 1:  # завтра
+        one_day = timedelta(days=2)  # потому что 17.05 нормальное расписание
+        return str(today + one_day)
+        # return '2021-05-17'
+
+
+def data_week_modifier(i):
+    today = date.today()
+    one_day = timedelta(days=i)
+    return str(today + one_day)
+
+
+def print_shedule(text, group_id):
+    t = parse_data.get_data(text, '99', group_id)
+    return t
+
+
+def abc(message):
+    global group
+
+    group = message.text
+    bot.send_message(message.chat.id, 'Твоя группа: ' + group)
+
+    return
 
 bot.polling()
